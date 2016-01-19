944e6b050756aa9d53891e5fd1161b41966788f8
vickyd885 <VickyD@Vickys-MacBook-Pro.local>
2016-01-19 23:49:10 +0000

    basic api for testing

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "OK"
+	return render_template("index.html")
+
+@app.route("/send/<message>")
+def recieve(message):
+	return render_template("recieve.html",message=message)
+
+@app.route("/get")
+def send():
+	return "Yes, I hear you loud and clear"
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
