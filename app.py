ef76b0ec7bf6c69ff58f2947f30d23d91924f8f6
vickyd885 <vickyd885@gmail.com>
2016-01-24 12:54:06 +0000

    changed get connection message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "test" #render_template("main/index.html")
+	return "Well done! You managed to connect to TechSoc's IoT Server" #render_template("main/index.html")
 
 @app.route("/send/<message>")
 def recieve(message):
