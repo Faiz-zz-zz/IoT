dd25635161ce79de627ae6c170e753f7c9eea335
vickyd885 <vickyd885@gmail.com>
2016-01-28 14:56:06 +0000

    test

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-
+from flask import request 
 app = Flask(__name__)
 
 @app.route("/")
@@ -17,11 +17,14 @@ def send_picture(link):
 
 
 @app.route("/testsomething",methods=['GET', 'POST'])
-def send_string(request):
+def send_string():
 	if request.method == 'POST':
-        print request.method['POST']
-    else 
-    	print request.method['GET']
+		print request.form['test']
+		return send_picture(request.form['test'])
+		#return render_template("main/picture.html",link=test)
+		#return "Well done! You managed to connect to TechSoc's IoT Server"
+	else:
+		return "Heard a get request"
 
 @app.route("/get")
 def send():
